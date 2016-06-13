ef75c8126a117b868f9c852342f11f3680863814
Christian Zagrodnick <cz@flyingcircus.io>
2016-06-13 17:02:28 +0200

    add ssh2 php lib

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -47,6 +47,19 @@ let self = with self; {
     ];
   };
 
+  ssh2 =  buildPecl {
+    name = "ssh2-0.13";
+    sha256 = "1gn1wqi3b4awmk0g701rrgj622lp9bm0dpx8v2y3cnqbhjmvhb6b";
+
+    configureFlags = [
+      "--with-ssh2=${pkgs.libssh2}"
+    ];
+
+    buildInputs = [
+       pkgs.libssh2
+    ];
+  };
+
 
   xdebug = buildPecl {
     name = "xdebug-2.3.1";
