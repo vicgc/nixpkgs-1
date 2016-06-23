fee7ae6f882d4c6b63b29b22d3ad118fbb52e0f0
Christian Kauhaus <kc@flyingcircus.io>
2016-06-23 15:33:28 +0200

    Improve source after code review
    - Delete counterproductive error handling in main().
    - Remove misleading "if __name__ == '__main__'" line. This module is not
      meant to be invoked as stand-alone script.
    - Rename dmidecodeparser into dmi_memory, since the module's primary
      purpose is to extract memory information from DMI.
    - Use pkg_resources to remove use of relative path names.
    - Install dmidecode at all.
    
    Re #21724

--- nixos/modules/flyingcircus/packages/fcmanage/src/fcmanage/tests/test_dmi_memory.py
+++ nixos/modules/flyingcircus/packages/fcmanage/src/fcmanage/tests/test_dmi_memory.py
@@ -1,18 +1,22 @@
 """ Unit-test for dmidecode parser"""
 
-from fcmanage.dmidecodeparser import calc_mem, get_paragraph, get_device
+from fcmanage.dmi_memory import calc_mem, get_paragraph, get_device
+import pkg_resources
 
 
 def test_calc_mem():
-    modules = [{'Size': '0MB'}, {'Size': '512MB'}, {'Size': '2048    MB'}, {'Size': '    9096 mb'}]
+    modules = [{'Size': '0MB'},
+               {'Size': '512MB'},
+               {'Size': '2048    MB'},
+               {'Size': '    9096 mb'}]
     res = calc_mem(modules)
     assert res == 11656
 
 
 def test_get_paragraph():
-    dmidecode_output = open('./src/fcmanage/tests/dd.output', 'r').readlines()
-    res = list(get_paragraph(dmidecode_output))
-    for rows in res:
+    with pkg_resources.resource_stream(__name__, 'dmidecode.out') as f:
+        dmidecode_output = f.readlines()
+    for rows in list(get_paragraph(dmidecode_output)):
         for elem in rows:
             assert elem != ""
             assert elem in dmidecode_output
