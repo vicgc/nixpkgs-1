532970d7c48e990ade5d1b993753e635964662c7
Christian Zagrodnick <cz@flyingcircus.io>
2016-06-27 20:40:17 +0200

    re-enable mysql tests and make sure root can login after db creation

--- nixos/modules/flyingcircus/tests/percona.nix
+++ nixos/modules/flyingcircus/tests/percona.nix
@@ -1,5 +1,3 @@
-# This test has been broken but still signaled "green" earlier on.
-# I have disabled it for now.
 import <nixpkgs/nixos/tests/make-test.nix> ({
     pkgs
     ,percona
@@ -41,12 +39,8 @@ import <nixpkgs/nixos/tests/make-test.nix> ({
     startAll;
 
     $master->waitForUnit("mysql");
-
-    # $master->sleep(10);
-    # This test was screwed and for some reason passed earlier on.
-    # This is only to ensure we build the packages, for now.
-    # $master->succeed("systemctl status sensu-server.service");
-    # $master->succeed("systemctl status sensu-api.service");
-    # $master->succeed("systemctl status uchiwa.service");
+    $master->sleep(1);
+    $master->succeed("mysqladmin ping");
+    $master->succeed("mysql mysql -e 'select * from user' > /dev/null");
   '';
 })
