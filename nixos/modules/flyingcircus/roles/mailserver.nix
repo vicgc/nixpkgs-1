d4d085365e5443a6b4655c509b1912e9af6b1219
Frank Lanitz <fl@flyingcircus.io>
2016-07-21 20:41:15 +0200

    Start to make mailserver role a little more extensible.
    This is adding a hook for a local.cf file container local settings as well as canonical.pcre
    
    Re #22490

--- nixos/modules/flyingcircus/roles/mailserver.nix
+++ nixos/modules/flyingcircus/roles/mailserver.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
 
   cfg = config.flyingcircus;
@@ -13,14 +11,25 @@ let
     config.flyingcircus.enc_services;
 
 
+  config_options = [
+    (if lib.pathExists "/etc/local/postfix/local.cf" then
+      lib.readFile /etc/local/postfix/local.cf
+     else "")
+
+    (if lib.pathExists "/etc/local/postfix/canonical.pcre" then
+      "canonical_maps = pcre:${/etc/local/postfix/canonical.pcre}\n"
+     else "")
+
+  ];
+
 in
 {
   options = {
 
     flyingcircus.roles.mailserver = {
 
-      enable = mkOption {
-        type = types.bool;
+      enable = lib.mkOption {
+        type = lib.types.bool;
         default = false;
         description = ''
           Enable the Flying Circus mailserver out role and configure
@@ -31,8 +40,9 @@ in
     };
   };
 
-  config = mkMerge [
-    (mkIf cfg.roles.mailserver.enable {
+  config = lib.mkMerge [
+
+   (lib.mkIf cfg.roles.mailserver.enable {
       services.postfix.enable = true;
 
       # Allow all networks on the SRV interface. We expect only trusted machines
@@ -41,9 +51,30 @@ in
         if cfg.enc.parameters.interfaces ? srv
         then builtins.attrNames cfg.enc.parameters.interfaces.srv.networks
         else [];
+
+      system.activationScripts.fcio-postfix = ''
+          install -d -o root -g service  -m 02775 /etc/local/postfix/
+        '';
+
+      environment.etc."local/postfix/README.txt".text = ''
+        Put your local postfix configuration here.
+
+        Use local.cf for pure configuration settings like
+        setting message_size_limit. Please do use normal main.cf syntax,
+        as this will extend the basic configuration file.
+
+        If you need to reference to some map, these are currently available:
+        * canonical.pcre
+
+        In case you need to extend this list, get in contact with our
+        support.
+      '';
+
+      services.postfix.extraConfig = lib.concatStringsSep "\n" config_options;
+
     })
 
-    (mkIf (!cfg.roles.mailserver.enable &&
+    (lib.mkIf (!cfg.roles.mailserver.enable &&
            mail_out_service != null) {
 
       networking.defaultMailServer.directDelivery = true;
