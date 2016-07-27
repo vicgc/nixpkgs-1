c5bcfda6cce860fefb62bb2694e49cfc67aed52f
Christian Kauhaus <kc@flyingcircus.io>
2016-07-27 15:05:51 +0200

    Fix fclib.files to tolerate missing directories
    fclib.files now returns an empty set if the specified path does not
    exist.

--- nixos/modules/flyingcircus/lib/files.nix
+++ nixos/modules/flyingcircus/lib/files.nix
@@ -1,14 +1,15 @@
-{ lib, fclib, ... }: with lib;
-
+{ lib, fclib, ... }:
+with lib;
 rec {
-
 
   # Get all regular files with their name relative to path
   filesRel = path:
-    (attrNames
-      (filterAttrs
-        (filename: type: (type == "regular"))
-        (builtins.readDir path)));
+    optionals
+      (pathExists path)
+      (attrNames
+        (filterAttrs
+          (filename: type: (type == "regular"))
+          (readDir path)));
 
   # Get all regular files with their absolute name
   files = path:
