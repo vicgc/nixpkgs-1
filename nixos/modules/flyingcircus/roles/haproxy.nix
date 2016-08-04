28c30aeefdb245a563756a861f14c57fb69a896a
Christian Kauhaus <kc@flyingcircus.io>
2016-08-04 17:31:10 +0200

    Improve handling of default configuration files
    Add fclib.configFromFile to capture a common pattern: use file if it
    exists, use default otherwise.
    
    Also improve default haproxy.cfg.
    
    Re #22535

--- nixos/modules/flyingcircus/roles/haproxy.nix
+++ nixos/modules/flyingcircus/roles/haproxy.nix
@@ -2,22 +2,38 @@
 
 let
   cfg = config.flyingcircus;
+  fclib = import ../lib;
 
-  nonEmptyString = string: (lib.stringLength string > 0);
-  configFromFile = file: default:
-    lib.findFirst (nonEmptyString) default [
-      (if (builtins.pathExists file)
-       then builtins.readFile file
-       else "")
-  ];
+  haproxyCfg = fclib.configFromFile /etc/local/haproxy/haproxy.cfg example;
 
-  haproxyCfg = configFromFile /etc/local/haproxy/haproxy.cfg ''
-    frontend http
-    bind 127.0.0.1:8002
-  '';
+  example = ''
+    # haproxy configuration example - copy to haproxy.cfg and adapt.
+
+    global
+        daemon
+        chroot /var/empty
+        user haproxy
+        group haproxy
+        maxconn 4096
+        log ::1 local2
+
+    defaults
+        mode http
+        log global
+        option httplog
+        option dontlognull
+        option http-server-close
+        timeout connect 5s
+        timeout client 30s    # should be equal to server timeout
+        timeout server 30s    # should be equal to client timeout
+        timeout queue 25s     # discard requests sitting too long in the queue
+
+    listen http-in
+        bind 127.0.0.1:8002
+        bind ::1:8002
+    '';
 
 in
-
 {
 
   options = {
@@ -39,7 +55,8 @@ in
     services.haproxy.enable = true;
     services.haproxy.config = haproxyCfg;
     system.activationScripts.haproxy = ''
-      install -d -o ${toString config.ids.uids.haproxy} -g service -m 02775 /etc/local/haproxy
+      install -d -o ${toString config.ids.uids.haproxy} -g service -m 02775 \
+        /etc/local/haproxy
     '';
 
     environment.etc = {
@@ -47,34 +64,9 @@ in
         HAProxy is enabled on this machine.
 
         Put your haproxy configuration here as `haproxy.cfg`. There is also
-        an `example-configuration` here.
+        an example configuration here.
       '';
-      "local/haproxy/example-configuration".text = ''
-      # haproxy configuration example - copy to haproxy.cfg and adapt.
-
-      global
-          daemon
-          chroot /var/empty
-          user haproxy
-          group haproxy
-          maxconn 4096
-
-      defaults
-          mode http
-          log global
-          option httplog
-          option dontlognull
-          option http-server-close
-          timeout connect 5s
-          timeout client 30s    # should be equal to server timeout
-          timeout server 30s    # should be equal to client timeout
-          timeout queue 25s   # discard requests sitting too long in the queue
-
-      listen http-in
-          bind 127.0.0.1:8002
-          bind ::1:8002
-
-      '';
+      "local/haproxy/haproxy.cfg.example".text = example;
       "haproxy.cfg" = {
         source = /etc/local/haproxy/haproxy.cfg;
         enable = cfg.compat.gentoo.enable;
