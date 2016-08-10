e7bde36ab4b2921e19b147a0e42b774cc72e2129
plumps <mb@flyingcircus.io>
2016-08-10 20:26:39 +0200

    Added loghost-client.nix role to send data to specific loghosts
    - Test it with flyingcircus.roles.loghost-client.enable = true; in local.nix

---
+++ nixos/modules/flyingcircus/roles/loghost-client.nix
@@ -0,0 +1,37 @@
+{ config, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.flyingcircus.roles.loghost-client;
+
+  loghost_server = lib.findFirst
+    (host: host.service == "loghost-server")
+    null
+    config.flyingcircus.enc_services;
+
+  graylog =
+    if loghost_server != null then
+      "*.* @${loghost_server.address}:10514;RSYSLOG_SyslogProtocol23Format"
+    else
+      "";
+
+in
+
+  {
+    options =  {
+      flyingcircus.roles.loghost-client = {
+        enable = mkOption {
+          type = types.bool;
+          default = false;
+          description = "Enable the flyingcircus loghost-client role";
+        };
+      };
+    };
+
+    config = mkIf (cfg.enable && loghost_server != null) {
+
+      flyingcircus.syslog.extraRules = graylog;
+    };
+  }
+
