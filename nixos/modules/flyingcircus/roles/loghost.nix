3d82ab9ec2fd621f393717b19fe4196554d7cee6
Christian Zagrodnick <cz@flyingcircus.io>
2016-09-05 13:41:38 +0200

    * bind graylog to srv address (the first one) * enable syslog forwarding for entire RG.

--- nixos/modules/flyingcircus/roles/loghost.nix
+++ nixos/modules/flyingcircus/roles/loghost.nix
@@ -1,5 +1,13 @@
 { config, lib, pkgs, ... }: with lib;
+let
+  fclib = import ../lib;
+  listenOn = head (fclib.listenAddresses config "ethsrv");
 
+  loghostService = lib.findFirst
+    (s: s.service == "loghost-server")
+    null
+    config.flyingcircus.enc_services;
+in
 {
 
   options = {
@@ -16,33 +24,38 @@
 
   };
 
-  config = mkIf config.flyingcircus.roles.loghost.enable {
+  config = mkMerge [
 
-  	networking.firewall.allowedTCPPorts = [ 9000 80 ];
+    (mkIf config.flyingcircus.roles.loghost.enable {
+      # XXX only SRV actually. And maybe not at all.
+    	networking.firewall.allowedTCPPorts = [ 9000 ];
 
-    services.graylog = {
-    	enable = true;
-	    passwordSecret = "admin";
-	    rootPasswordSha2 = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
-	    elasticsearchClusterName = "graylog";
-	    extraConfig = ''
-	    '';
-  	};
-  	services.elasticsearch2 = {
-    	enable = true;
-    	cluster_name = "graylog";
-    	extraConf = ''
-        discovery_zen_ping_multicast_enabled : false
-        # List of Elasticsearch nodes to connect to
-        elasticsearch_discovery_zen_ping_unicast_hosts : localhost:9300
+      services.graylog = {
+      	enable = true;
+        # XXX move out of here
+  	    passwordSecret = "gangland-daresay-polarize-celsius";
+        # "admin"
+  	    rootPasswordSha2 = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
+  	    elasticsearchClusterName = "graylog";
+  	    extraConfig = ''
+          # IPv6 would be nice, too :/
+          web_listen_uri http://${listenOn}:9000/
+  	    '';
+    	};
+    	services.elasticsearch2 = {
+      	enable = true;
+      	cluster_name = "graylog";
+      	extraConf = ''
+          discovery_zen_ping_multicast_enabled : false
+          # List of Elasticsearch nodes to connect to
+          elasticsearch_discovery_zen_ping_unicast_hosts : localhost:9300
+        '';
+    	};
+    	flyingcircus.roles.mongodb.enable = true;
+    })
+    (mkIf (loghostService != null) {
+      services.rsyslogd.extraConfig = ''
+        *.* @${loghostService.address}:5140;RSYSLOG_SyslogProtocol23Format
       '';
-  	};
-    services.rsyslogd.extraConfig = ''
-      *.* @127.0.0.1:5140;RSYSLOG_SyslogProtocol23Format
-    '';
-
-  	flyingcircus.roles.webgateway.enable = true;
-  	flyingcircus.roles.webproxy.enable = true;
-  	flyingcircus.roles.mongodb.enable = true;
-  };
+    })];
 }
