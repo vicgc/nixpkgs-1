b4bb13e891e35900fcb481ebf4be77b23e1a7eab
Christian Zagrodnick <cz@flyingcircus.io>
2016-09-26 15:27:49 +0200

    prepare monitoring
    re #23966

--- nixos/modules/flyingcircus/packages/sensu/update.sh
+++ nixos/modules/flyingcircus/packages/sensu/update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -x
+NIX_PATH="nixpkgs=$(dirname $0)/../../../../../"
 rm -f Gemfile.lock
 rm -rf /tmp/bundix
-nix-shell -p git -p stdenv -p cacert -p bundler -p openssl --command "bundler package --no-install --path /tmp/bundix/bundle"
+nix-shell -p git -p stdenv -p stdenv -p cacert -p bundler -p openssl --command "bundler package --no-install --path /tmp/bundix/bundle"
 nix-shell -p bundix --command bundix
