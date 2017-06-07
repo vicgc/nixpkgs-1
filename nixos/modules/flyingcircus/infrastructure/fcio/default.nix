ca6a2060f21cafe337629ce2591048e9c76accdd
Christian Kauhaus <kc@flyingcircus.io>
2017-06-07 21:51:56 +0200

    Add root key for KC
    Re #27045

--- nixos/modules/flyingcircus/infrastructure/fcio/default.nix
+++ nixos/modules/flyingcircus/infrastructure/fcio/default.nix
@@ -96,6 +96,7 @@ in
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrMeeyMUiSfXGnhvdIk50RsW3VMAbmYAChOGmiKGMUc ctheune@thirteen.fritz.box"
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSejGFORJ7hlFraV3caVir3rWlo/QcsWptWrukk2C7eaGu/8tXMKgPtBHYdk4DYRi7EcPROllnFVzyVTLS/2buzfIy7XDjn7bwHzlHoBHZ4TbC9auqW3j5oxTDA4s2byP6b46Dh93aEP9griFideU/J00jWeHb27yIWv+3VdstkWTiJwxubspNdDlbcPNHBGOE+HNiAnRWzwyj8D0X5y73MISC3pSSYnXJWz+fI8IRh5LSLYX6oybwGX3Wu+tlrQjyN1i0ONPLxo5/YDrS6IQygR21j+TgLXaX8q8msi04QYdvnOqk1ntbY4fU8411iqoSJgCIG18tOgWTTOcBGcZX directory@directory.fcio.net"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB6MKl9D9mzhuB6/sQXNCEW5qq4R7mXlpnxi+QZSGi57 root/ckauhaus@fcio.net"
     ];
   };
 
