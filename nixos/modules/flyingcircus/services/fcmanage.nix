vicgc/nixpkgs-1

commit 7f754110597d60a329dd7d3a72f8a5337305795e

Migration path for the new feature switch
The code now obeys both the old feature flag
(/etc/local/build-with-maintenance file) and the new feature switch
(config.flyingcircus.agent.with-maintenance option). If the old feature
flag is present, a warning is emitted so that alert admins may change
it to the new feature switch.

Re #26699

nixos/modules/flyingcircus/services/fcmanage.nix
@@ -7,14 +7,21 @@ with lib;
 
 let
   cfg = config.flyingcircus;
-  defaultUpdateAction =
-    if cfg.agent.with-maintenance then "--channel-with-maintenance" else "--channel";
+
+  # migration for #26699
+  deprecatedBuildWithMaintenanceFlag =
+    builtins.pathExists "/etc/local/build-with-maintenance";
+
+  channelAction =
+    if deprecatedBuildWithMaintenanceFlag || cfg.agent.with-maintenance
+    then "--channel-with-maintenance"
+    else "--channel";
 
 in {
   options = {
-
     flyingcircus.agent = {
-      enable = mkEnableOption "automatic runs of the Flying Circus management agent";
+      enable = mkEnableOption
+        "automatic runs of the Flying Circus management agent";
 
       with-maintenance = mkOption {
         default = false;
@@ -24,15 +31,21 @@ in {
 
       steps = mkOption {
         type = types.str;
-        default = "--directory --system-state --maintenance ${defaultUpdateAction}";
+        default = "--directory --system-state --maintenance ${channelAction}";
         description = "Steps to run by the agent.";
       };
     };
-
   };
 
   config = mkMerge [
     {
+      # migration for #26699
+      warnings = lib.optional deprecatedBuildWithMaintenanceFlag ''
+        Deprecated "build-with-maintenance" flag file detected.
+        Set NixOS option "flyingcircus.agent.with-maintenance" instead and
+        delete the old flag file to get rid of this warning.
+      '';
+
       # We always install the management agent, but we don't necessarily
       # enable it running automatically.
       environment.systemPackages = [
@@ -73,11 +86,9 @@ in {
         %sudo-srv ALL=(root) FCMANAGE
         %service  ALL=(root) FCMANAGE
       '';
-
     }
 
     (mkIf config.flyingcircus.agent.enable {
-
       systemd.timers.fc-manage = {
         description = "Timer for fc-manage";
         wantedBy = [ "timers.target" ];
@@ -88,7 +99,6 @@ in {
           # RandomSec = "3m";
         };
       };
-
     })
   ];
 }

commit 84c951a46c82e275ab8fcf202cf257f6082e7398

Fix regression introduced in 7f7541105
We cannot use 'mkEnableOption' since it sets the default to 'false'.

Re #26699

nixos/modules/flyingcircus/services/fcmanage.nix
@@ -20,13 +20,16 @@ let
 in {
   options = {
     flyingcircus.agent = {
-      enable = mkEnableOption
-        "automatic runs of the Flying Circus management agent";
+      enable = mkOption {
+        default = true;  # <-!!!
+        description = "Run the Flying Circus management agent automatically.";
+        type = types.bool;
+      };
 
       with-maintenance = mkOption {
         default = false;
         description = "Perform NixOS updates in scheduled maintenance.";
-        type = lib.types.bool;
+        type = types.bool;
       };
 
       steps = mkOption {
@@ -88,7 +91,7 @@ in {
       '';
     }
 
-    (mkIf config.flyingcircus.agent.enable {
+    (mkIf cfg.agent.enable {
       systemd.timers.fc-manage = {
         description = "Timer for fc-manage";
         wantedBy = [ "timers.target" ];