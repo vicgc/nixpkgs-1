b7a5b4633e6dc661302aa56278bf9ce128686b4b
Christian Zagrodnick <cz@flyingcircus.io>
2017-08-17 11:20:29 +0200

    gather haproxy stats
    re #27132

--- nixos/modules/flyingcircus/roles/haproxy.nix
+++ nixos/modules/flyingcircus/roles/haproxy.nix
@@ -5,6 +5,7 @@ let
   fclib = import ../lib;
 
   haproxyCfg = fclib.configFromFile /etc/local/haproxy/haproxy.cfg example;
+  statsSocket = "/run/haproxy_admin.sock";
 
   example = ''
     # haproxy configuration example - copy to haproxy.cfg and adapt.
@@ -16,6 +17,7 @@ let
         group haproxy
         maxconn 4096
         log localhost local2
+        stats socket ${statsSocket} mode 660 group nogroup level operator
 
     defaults
         mode http
@@ -54,7 +56,9 @@ in
 
   };
 
-  config = mkIf config.flyingcircus.roles.haproxy.enable {
+  config = mkMerge [
+
+  (mkIf config.flyingcircus.roles.haproxy.enable {
 
     services.haproxy.enable = true;
     services.haproxy.config = haproxyCfg;
@@ -84,6 +88,43 @@ in
     flyingcircus.syslog.separateFacilities = {
       local2 = "/var/log/haproxy.log";
     };
-  };
 
+    systemd.services.prometheus-haproxy-exporter = {
+      description = "Prometheus exporter for haproxy metrics";
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.haproxy ];
+      script = ''
+        exec ${pkgs.prometheus-haproxy-exporter}/bin/haproxy_exporter \
+          -web.listen-address localhost:9127 \
+          -haproxy.scrape-uri=unix:${statsSocket}
+      '';
+      serviceConfig = {
+        User = "nobody";
+        Restart = "always";
+        PrivateTmp = true;
+        WorkingDirectory = /tmp;
+        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+      };
+    };
+
+    services.telegraf.inputs = {
+      prometheus  = [{
+        urls = ["http://localhost:9127/metrics"];
+      }];
+    };
+  })
+
+ {
+    flyingcircus.roles.statshost.prometeusMetricRelabel = [
+      # Remove _counter and _gauge postfixes which telegraf adds. See
+      # https://github.com/influxdata/telegraf/issues/2950
+      { source_labels = ["__name__"];
+       regex = "haproxy_(.+)_(counter|gauge)";
+       replacement = "haproxy_\${1}";
+       target_label = "__name__";
+      }
+    ];
+  }
+
+  ];
 }
