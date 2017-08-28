506abcb2a52053246a1870688ef60bd5eea6a4b6
Christian Kauhaus <kc@flyingcircus.io>
2017-08-28 17:01:41 +0200

    Increase source readability
    Also increase port check interval a bit.
    
    Re #28242

--- nixos/modules/flyingcircus/roles/postgresql.nix
+++ nixos/modules/flyingcircus/roles/postgresql.nix
@@ -117,18 +117,24 @@ in
     systemd.services.postgresql.after = [ "network-interfaces.target" ];
     systemd.services.postgresql.wants = [ "network-interfaces.target" ];
 
-
-    systemd.services.postgresql.postStart = ''
-      { ${postgresqlPkg}/bin/psql -c '\du' template1 | grep -q '^ *nagios *|'; } || ${postgresqlPkg}/bin/psql -c 'CREATE ROLE nagios NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN' template1
-
-      { ${postgresqlPkg}/bin/psql -l | grep -q '^ *nagios *|'; } || ${postgresqlPkg}/bin/createdb nagios
-      ${postgresqlPkg}/bin/psql -q -d nagios -c 'REVOKE ALL ON SCHEMA public FROM PUBLIC CASCADE;'
+    systemd.services.postgresql.postStart =
+    let
+      psql = "${postgresqlPkg}/bin/psql";
+    in ''
+      if ! ${psql} -c '\du' template1 | grep -q '^ *nagios *|'; then
+        ${psql} -c 'CREATE ROLE nagios NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN' template1
+      fi
+      if ! ${psql} -l | grep -q '^ *nagios *|'; then
+        ${postgresqlPkg}/bin/createdb nagios
+      fi
+      ${psql} -q -d nagios -c 'REVOKE ALL ON SCHEMA public FROM PUBLIC CASCADE;'
     '';
 
     users.users.postgres = {
       shell = "/run/current-system/sw/bin/bash";
       home = "/srv/postgresql";
     };
+
     system.activationScripts.flyingcircus_postgresql = ''
       install -d -o ${toString config.ids.uids.postgres} /srv/postgresql
       install -d -o ${toString config.ids.uids.postgres} -g service -m 02775 \
@@ -225,13 +231,13 @@ in
       # authenticated access for others
       host all  all  0.0.0.0/0  md5
       host all  all  ::/0       md5
-
     '';
 
     flyingcircus.services.sensu-client.checks = {
       postgresql = {
         notification = "PostgreSQL alive";
-        command =  "/var/setuid-wrappers/sudo -u postgres check-postgres-alive.rb -d postgres";
+        command = "/var/setuid-wrappers/sudo -u postgres check-postgres-alive.rb -d postgres";
+        interval = 120;
       };
     } // lib.listToAttrs (
       map (host:
@@ -240,8 +246,9 @@ in
           { name = "postgresql-listen-${sane_host}-5432";
             value = {
               notification = "PostgreSQL listening on ${host}:5432";
-              command =  " check-postgres-alive.rb -h ${host} -u nagios -d nagios -P 5432";
-              };
+              command = "check-postgres-alive.rb -h ${host} -u nagios -d nagios -P 5432";
+              interval = 120;
+            };
           })
         listen_addresses);
 
@@ -252,5 +259,4 @@ in
     };
 
   });
-
 }
