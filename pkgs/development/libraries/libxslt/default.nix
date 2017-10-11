ab0b6fe99f936647a75b0919e2b0c41e5611ad48
Christian Theune <ct@flyingcircus.io>
2017-10-11 15:34:05 +0200

    Security updates for libxml and libxslt.
    libxml: 2.9.4 -> 2.9.6
    libxslt: 1.1.29 -> 1.1.31
    
    Fixes #28608
    Fixes #28609

--- pkgs/development/libraries/libxslt/default.nix
+++ pkgs/development/libraries/libxslt/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, fetchpatch, libxml2, findXMLCatalogs }:
 
 stdenv.mkDerivation rec {
-  name = "libxslt-1.1.29";
+  name = "libxslt-1.1.31";
 
   src = fetchurl {
     url = "http://xmlsoft.org/sources/${name}.tar.gz";
-    sha256 = "1klh81xbm9ppzgqk339097i39b7fnpmlj8lzn8bpczl3aww6x5xm";
+    sha256 = "1ddh81xbm9ppzgqk339097i39b7fnpmlj8lzn8bpczl3aww6x5xm";
   };
 
   patches = stdenv.lib.optional stdenv.isSunOS ./patch-ah.patch;
