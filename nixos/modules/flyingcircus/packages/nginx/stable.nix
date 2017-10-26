8161a56150eedc8dbfa5e19452faa7b1aa42ac86
Frank Lanitz <fl@flyingcircus.io>
2017-10-26 10:38:55 +0200

    Update Nginx to 0.10.3

--- nixos/modules/flyingcircus/packages/nginx/stable.nix
+++ nixos/modules/flyingcircus/packages/nginx/stable.nix
@@ -1,6 +1,6 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "1.10.2";
-  sha256 = "1hk5szkwns6s0xsvd0aiy392sqbvk3wdl480bpxf55m3hx4sqi8h";
+  version = "1.10.3";
+  sha256 = "75020f1364cac459cb733c4e1caed2d00376e40ea05588fb8793076a4c69dd90";
 })
