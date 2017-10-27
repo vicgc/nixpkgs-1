3223da4cd03973ca7ccc79bf5771a819ad2d4b14
Christian Zagrodnick <cz@flyingcircus.io>
2017-10-27 09:55:48 +0200

    add prebuild for php wiht zts

---
+++ nixos/modules/flyingcircus/tests/phpzts.nix
@@ -0,0 +1,30 @@
+# Some extensions (like redis) require ZTS in PHP. We don't know if it's wise
+# to enable it globally. So we allow it to be customized via
+# `.nixpkgs/config.nix`. Building PHP takes quite a while. This test causes
+# a pre-built PHP with ZTS enabled as a side-effect.
+import ../../../tests/make-test.nix ({ lib, pkgs, ... }:
+{
+  name = "phpzts";
+  machine =
+    { config, ... }:
+    {
+      imports = [
+        ./setup.nix
+        ../static
+        ../roles
+        ../services
+        ../platform
+      ];
+
+    };
+  testScript =
+    let
+      php56 = (pkgs.php56.override { config.php.zts = true; });
+      php70 = (pkgs.php70.override { config.php.zts = true; });
+    in
+    ''
+      startAll;
+      $machine->succeed("${php56}/bin/php -i | grep no-debug-zts >/dev/console");
+      $machine->succeed("${php70}/bin/php -i | grep no-debug-zts >/dev/console");
+    '';
+})
